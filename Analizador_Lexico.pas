f6c8eecf177b40652e86cef81874fbb6b889c6e2
RenatoBonnin <rebonato.bonnin@hotmail.com>
2026-05-01 04:10:04 -0300

    Funciones EsOperador,Real,etc

--- Analizador_Lexico.pas
+++ Analizador_Lexico.pas
@@ -68,7 +68,7 @@ Begin
   control:=1; //inicializamos el recorrido de la cadena desde el primer caracter
   While (Control <= Length(Cadena)) and (EstadoActual <> 2) do //lo recorremos hasta que termine la cadena o bien llegue al estado muerto
   Begin
-    EstadoActual:=Delta[EstadoActual,CarASimb(Cadena[Control])];
+    EstadoActual:=Delta[EstadoActual,CarASimbId(Cadena[Control])];
     Control:=Control+1;
   end;
 
@@ -76,14 +76,219 @@ Begin
   EsIdentificador:=EstadoActual in F;
 End;
 
-Function CarASimb(Car:Char):Sigma;
+Function CarASimbId(Car:Char):Sigma;
 Begin
 //Aqui cargamos todos los simbolos que debe reconocer segun su categoria
   Case Car of
-    'a'..'z', 'A'..'Z':CarASimb:=Letra;
-    '0'..'9'          :CarASimb:=Digito;
-    '_'               :CarASimb:=Guion;
+    'a'..'z', 'A'..'Z':CarASimbId:=Letra;
+    '0'..'9'          :CarASimbId:=Digito;
+    '_'               :CarASimbId:=Guion;
   else
-    CarASimb:=Otro
+    CarASimbId:=Otro
   End;
+End;
+
+Function CarASimbReal(Car:Char):Sigma;
+Begin
+//Aqui cargamos todos los simbolos que debe reconocer segun su categoria
+  Case Car of
+    '0'..'9'          :CarASimbReal:=Digito;
+    'e'               :CarASimbReal:=Exp;
+    '.'               :CarASimbReal:=Punto;
+    '-'               :CarASimbReal:=Neg;
+  else
+    CarASimbReal:=Otro
+  End;
+End;
+
+Function CarASimbRelacional(Car:Char):Sigma;
+Begin
+//Aqui cargamos todos los simbolos que debe reconocer segun su categoria
+  Case Car of
+    '<'          :CarASimbReal:=Menor;
+    '>'          :CarASimbReal:=Mayor;
+    '='          :CarASimbReal:=Igual;
+  else
+    CarASimbReal:=Otro
+  End;
+End;
+
+Function EsConstanteReal(Cadena:String):Boolean;
+Const
+  F=[1, 9, 6]; //Conjunto de estados finales
+Type
+  Q=0..11; //Numero de estados
+  Sigma=(Digito, Exp, Punto, Neg, Otro);
+  TipoDelta=Array[Q,Sigma] of Q;
+Var
+  Control:Integer; //Control para recorrer la cadena de entrada
+  EstadoActual:Q;
+  Delta:TipoDelta;
+Begin
+  {Aca definimos la función de transición, para cada estado y cada símbolo del alfabeto,
+   indicando a qué estado se transita, en el caso del estado muerto, no colocamos transiciones
+   para que al primer contacto, ya no acepte la cadena}
+  {DIGITO, EXPONENTE, PUNTO, NEGATIVO, OTRO}
+  Delta[0,Digito]:=1;Delta[1,Digito]:=1;Delta[2,Digito]:=1;Delta[3,Digito]:=4;
+  Delta[5,Digito]:=6;Delta[6,Digito]:=6;Delta[7,Digito]:=6;Delta[8,Digito]:=9;
+  Delta[9,Digito]:=9;Delta[10,Digito]:=9;
+  
+  Delta[0,Exp]:=11; Delta[1,Exp]:=8;Delta[2,Exp]:=11;Delta[3,Exp]:=11;Delta[4,Exp]:=5;
+  Delta[5,Exp]:=11;Delta[6,Exp]:=11;Delta[7,Exp]:=11;Delta[8,Exp]:=11;Delta[9,Exp]:=11;
+  Delta[10,Exp]:=11;
+  
+  Delta[0,Punto]:=11;Delta[1,Punto]:=3;Delta[2,Punto]:=11;Delta[3,Punto]:=11;
+  Delta[4,Punto]:=11;Delta[5,Punto]:=11;Delta[6,Punto]:=11;Delta[7,Punto]:=11;
+  Delta[8,Punto]:=11;Delta[9,Punto]:=11;Delta[10,Punto]:=11;
+  
+  Delta[0,Neg]:=2;Delta[1,Neg]:=11;Delta[2,Neg]:=11;Delta[3,Neg]:=11;Delta[4,Neg]:=11;Delta[5,Neg]:=7;
+  Delta[6,Neg]:=11;Delta[7,Neg]:=11;Delta[8,Neg]:=10;Delta[9,Neg]:=11;Delta[10,Neg]:=11;
+
+  Delta[0,Otro]:=11;Delta[1,Otro]:=11;Delta[2,Otro]:=11;Delta[3,Otro]:=11;Delta[4,Otro]:=11;Delta[5,Otro]:=11;
+  Delta[6,Otro]:=11;Delta[7,Otro]:=11;Delta[8,Otro]:=11;Delta[9,Otro]:=11;Delta[10,Otro]:=11;
+
+  EstadoActual:=0; //colocamos el estado inicial
+  control:=1; //inicializamos el recorrido de la cadena desde el primer caracter
+  
+  While (Control <= Length(Cadena)) and (EstadoActual <> 11) do //lo recorremos hasta que termine la cadena o bien llegue al estado muerto
+  
+  Begin
+    EstadoActual:=Delta[EstadoActual,CarASimbReal(Cadena[Control])];
+    Control:=Control+1;
+  end;
+
+//Si llego al estado final devuelve verdadero, caso contrario devuelve falso
+  EsConstanteReal:=EstadoActual in F;
+End;
+
+{
+Function EsConstanteEntera(Cadena:String):Boolean;
+Const
+  F=[1, 9, 6]; //Conjunto de estados finales
+Type
+  Q=0..11; //Numero de estados
+  Sigma=(Digito, Exp, Punto, Neg, Otro);
+  TipoDelta=Array[Q,Sigma] of Q;
+Var
+  Control:Integer; //Control para recorrer la cadena de entrada
+  EstadoActual:Q;
+  Delta:TipoDelta;
+Begin
+  //DIGITO, EXPONENTE, PUNTO, NEGATIVO, OTRO
+  Delta[0,Digito]:=1;Delta[1,Digito]:=1;Delta[2,Digito]:=1;Delta[3,Digito]:=4;
+  Delta[5,Digito]:=6;Delta[6,Digito]:=6;Delta[7,Digito]:=6;Delta[8,Digito]:=9;
+  Delta[9,Digito]:=9;Delta[10,Digito]:=9;
+  
+  Delta[0,Exp]:=11; Delta[1,Exp]:=8;Delta[2,Exp]:=11;Delta[3,Exp]:=11;Delta[4,Exp]:=5;
+  Delta[5,Exp]:=11;Delta[6,Exp]:=11;Delta[7,Exp]:=11;Delta[8,Exp]:=11;Delta[9,Exp]:=11;
+  Delta[10,Exp]:=11;
+  
+  Delta[0,Punto]:=11;Delta[1,Punto]:=3;Delta[2,Punto]:=11;Delta[3,Punto]:=11;
+  Delta[4,Punto]:=11;Delta[5,Punto]:=11;Delta[6,Punto]:=11;Delta[7,Punto]:=11;
+  Delta[8,Punto]:=11;Delta[9,Punto]:=11;Delta[10,Punto]:=11;
+  
+  Delta[0,Neg]:=2;Delta[1,Neg]:=11;Delta[2,Neg]:=11;Delta[3,Neg]:=11;Delta[4,Neg]:=11;Delta[5,Neg]:=7;
+  Delta[6,Neg]:=11;Delta[7,Neg]:=11;Delta[8,Neg]:=10;Delta[9,Neg]:=11;Delta[10,Neg]:=11;
+
+  Delta[0,Otro]:=11;Delta[1,Otro]:=11;Delta[2,Otro]:=11;Delta[3,Otro]:=11;Delta[4,Otro]:=11;Delta[5,Otro]:=11;
+  Delta[6,Otro]:=11;Delta[7,Otro]:=11;Delta[8,Otro]:=11;Delta[9,Otro]:=11;Delta[10,Otro]:=11;
+
+  EstadoActual:=0; //colocamos el estado inicial
+  control:=1; //inicializamos el recorrido de la cadena desde el primer caracter
+  
+  While (Control <= Length(Cadena)) and (EstadoActual <> 11) do //lo recorremos hasta que termine la cadena o bien llegue al estado muerto
+  
+  Begin
+    EstadoActual:=Delta[EstadoActual,CarASimbEnt(Cadena[Control])];
+    Control:=Control+1;
+  end;
+
+//Si llego al estado final devuelve verdadero, caso contrario devuelve falso
+  EsConstanteEntera:=EstadoActual in F;
+End;
+
+
+Function EsConstanteCadena(Cadena:String):Boolean;
+Const
+  F=[1, 9, 6]; //Conjunto de estados finales
+Type
+  Q=0..11; //Numero de estados
+  Sigma=(Digito, Exp, Punto, Neg, Otro);
+  TipoDelta=Array[Q,Sigma] of Q;
+Var
+  Control:Integer; //Control para recorrer la cadena de entrada
+  EstadoActual:Q;
+  Delta:TipoDelta;
+Begin
+
+  Delta[0,Digito]:=1;Delta[1,Digito]:=1;Delta[2,Digito]:=1;Delta[3,Digito]:=4;
+  Delta[5,Digito]:=6;Delta[6,Digito]:=6;Delta[7,Digito]:=6;Delta[8,Digito]:=9;
+  Delta[9,Digito]:=9;Delta[10,Digito]:=9;
+  
+  Delta[0,Exp]:=11; Delta[1,Exp]:=8;Delta[2,Exp]:=11;Delta[3,Exp]:=11;Delta[4,Exp]:=5;
+  Delta[5,Exp]:=11;Delta[6,Exp]:=11;Delta[7,Exp]:=11;Delta[8,Exp]:=11;Delta[9,Exp]:=11;
+  Delta[10,Exp]:=11;
+  
+  Delta[0,Punto]:=11;Delta[1,Punto]:=3;Delta[2,Punto]:=11;Delta[3,Punto]:=11;
+  Delta[4,Punto]:=11;Delta[5,Punto]:=11;Delta[6,Punto]:=11;Delta[7,Punto]:=11;
+  Delta[8,Punto]:=11;Delta[9,Punto]:=11;Delta[10,Punto]:=11;
+  
+  Delta[0,Neg]:=2;Delta[1,Neg]:=11;Delta[2,Neg]:=11;Delta[3,Neg]:=11;Delta[4,Neg]:=11;Delta[5,Neg]:=7;
+  Delta[6,Neg]:=11;Delta[7,Neg]:=11;Delta[8,Neg]:=10;Delta[9,Neg]:=11;Delta[10,Neg]:=11;
+
+  Delta[0,Otro]:=11;Delta[1,Otro]:=11;Delta[2,Otro]:=11;Delta[3,Otro]:=11;Delta[4,Otro]:=11;Delta[5,Otro]:=11;
+  Delta[6,Otro]:=11;Delta[7,Otro]:=11;Delta[8,Otro]:=11;Delta[9,Otro]:=11;Delta[10,Otro]:=11;
+
+  EstadoActual:=0; //colocamos el estado inicial
+  control:=1; //inicializamos el recorrido de la cadena desde el primer caracter
+  
+  While (Control <= Length(Cadena)) and (EstadoActual <> 11) do //lo recorremos hasta que termine la cadena o bien llegue al estado muerto
+  
+  Begin
+    EstadoActual:=Delta[EstadoActual,CarASimbCad(Cadena[Control])];
+    Control:=Control+1;
+  end;
+
+//Si llego al estado final devuelve verdadero, caso contrario devuelve falso
+  EsConstanteReal:=EstadoActual in F;
+End;
+}
+
+Function EsOperadorRelacional(Cadena:String):Boolean;
+Const
+  F=[1,2,3,4,5]; //Conjunto de estados finales
+Type
+  Q=0..6; //Numero de estados
+  Sigma=(Menor, Mayor, Igual, Otro);
+  TipoDelta=Array[Q,Sigma] of Q;
+Var
+  Control:Integer; //Control para recorrer la cadena de entrada
+  EstadoActual:Q;
+  Delta:TipoDelta;
+Begin
+
+  Delta[0,Menor]:=1;Delta[1,Menor]:=6;Delta[2,Menor]:=6;Delta[3,Menor]:=6;
+  Delta[4,Menor]:=6;Delta[5,Menor]:=6;
+  
+  Delta[0,Mayor]:=2;Delta[1,Mayor]:=4;Delta[2,Mayor]:=6;Delta[3,Mayor]:=6;
+  Delta[4,Mayor]:=6;Delta[5,Mayor]:=6;
+  
+  Delta[0,Igual]:=3;Delta[1,Igual]:=5;Delta[2,Igual]:=5;Delta[3,Igual]:=6;
+  Delta[4,Igual]:=6;Delta[5,Igual]:=6;
+
+  Delta[0,Otro]:=6;Delta[1,Otro]:=6;Delta[2,Otro]:=6;Delta[3,Otro]:=6;
+  Delta[4,Otro]:=6;Delta[5,Otro]:=6;
+
+  EstadoActual:=0; //colocamos el estado inicial
+  control:=1; //inicializamos el recorrido de la cadena desde el primer caracter
+  
+  While (Control <= Length(Cadena)) and (EstadoActual <> 6) do //lo recorremos hasta que termine la cadena o bien llegue al estado muerto
+  
+  Begin
+    EstadoActual:=Delta[EstadoActual,CarASimbReal(Cadena[Control])];
+    Control:=Control+1;
+  end;
+
+//Si llego al estado final devuelve verdadero, caso contrario devuelve falso
+  EsOperadorRelacional:=EstadoActual in F;
 End;
